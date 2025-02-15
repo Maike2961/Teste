6d0bf12b42ea51e981b9b39dd8b19ff691e0babc
mayki <mayki.silva@hotmail.com>
2025-02-15 12:58:16 -0300

    docs: explicação a aplicação

--- app.py
+++ app.py
@@ -3,18 +3,26 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+""" Rota com metodo Http Get """
 @app.route("/saudacao", methods=["GET"])
 def getNome():
+    """ Pegando o valor inserido na url da rota com request """
     nome = request.args.get("nome")
+    """ retornando o nome instânciado com json com jsonify """
     return jsonify({"message": f"saudação {nome}"})
 
+""" Rota com metodo Http Post """
 @app.route("/soma", methods=["POST"])
 def postSoma():
+    """ recebendo os valores enviados via post """
     number = request.get_json()
     
+    """ pegando os valores pelas chaves do json e instânciando para uma nova variavel e somando """
     n1 = number.get("n1")
     n2 = number.get("n2")
     soma = n1 + n2
+    """ retornando a soma dos valores com json"""
     return jsonify({"message": f"{soma}"})
 
 
