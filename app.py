3b84d1bbf10024e4abda4d230747472b5293b2fb
mayki <mayki.silva@hotmail.com>
2025-02-15 15:18:36 -0300

    docs: machine learning

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 """ 
-    Durante meu estagio em 2023, surgiu um projeto de desenvolver um aplicativo em Dart e Flutter
-    uma linguagem e Framework que eu nunca tive contato, e não tive layout ou designers sobre como 
-    o aplicativo em si ficaria, precisei desenvolver tanto o backend quanto o frontend dessa aplicação e 
-    enquanto eu estava aprendendo, eu estava desenvolvendo e meu gerente sempre me pedindo atualizações 
-    sobre o andamento do projeto, foi uma experiência desafiadora em que tive atrasos, devido a bugs e 
-    erros de design mas que no fim foi entregue um aplicativo satisfatório, e consegui devido a gestão de 
-    tempo entre aprendizado e realização dos codigos e design e a calma acima para conseguir coordenar
-    as tarefas. 
+    O impacto de um dataset debalanceado seria a classe dominante
+    vai se sobressair comparada a classe menor, um solucão para isso
+    seria reduzir o numeor de observações da classe dominante para diminuir
+    a diferença entre eles.
 
 """
     
